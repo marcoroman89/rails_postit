91007a9820cf3273398f5cd79d8bd6d7fa91547d
Marco Roman <marco.roman89@gmail.com>
2013-11-23 18:53:23 -0600

    Updated posts controller with new and create methods. Created model backed form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,27 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit!)
+
+    if @post.save
+      flash[:notice] = "You created a new post!"
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
 end
