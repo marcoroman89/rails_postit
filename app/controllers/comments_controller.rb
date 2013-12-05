2ff8d2b566a278b47db166d763f9848f7f91e73d
Marco Roman <marco.roman89@gmail.com>
2013-12-04 23:38:14 -0600

    Addev oting capabilites to comments and ability to edit and update comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,15 +15,32 @@ class CommentsController < ApplicationController
   end
 
   def edit
-
+    @post = Post.find(params[:post_id])
   end
 
   def update
+    @post = Post.find(params[:post_id])
     if @comment.update(comment_params)
       flash[:notice] = "You updated your comment!"
       redirect_to post_path(@post)
     else
       render :edit
+    end
+  end
+
+  def vote
+    @comment = Comment.find(params[:id])
+    @vote = Vote.create(voteable: @comment, creator: current_user, vote: params[:vote])
+
+    respond_to do |format|
+      format.html do
+        flash[:notice] = "Your vote was counted."
+        redirect_to :back
+      end
+
+      format.js do
+        render :vote
+      end
     end
   end
 
